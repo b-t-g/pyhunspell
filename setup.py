9a7c6dcb4501c7e5c3896b663d3a62a5e18cc796
Brendan <brendan@example.com>
2025-11-06 13:38:11 -0500

    Update Homebrew paths for installing on Mac.

--- setup.py
+++ setup.py
@@ -36,12 +36,13 @@ if platform.system() == "Windows":
 elif platform.system() == "Darwin":
     main_module_kwargs['define_macros'] = [('_LINUX', None)]
     main_module_kwargs['libraries'] = ['hunspell']
-    main_module_kwargs['include_dirs'] = '/usr/local/Cellar/hunspell/1.6.2/include/hunspell',
+    main_module_kwargs['library_dirs'] = ['/opt/homebrew/opt/hunspell/lib']
+    main_module_kwargs['include_dirs'] = ['/opt/homebrew/opt/hunspell/include/hunspell']
     main_module_kwargs['extra_compile_args'] = ['-Wall']
 else:
     main_module_kwargs['define_macros'] = [('_LINUX', None)]
     main_module_kwargs['libraries'] = ['hunspell']
-    main_module_kwargs['include_dirs'] = get_linux_include_dirs() + ['/usr/include/hunspell']
+    main_module_kwargs['include_dirs'] = get_linux_include_dirs() + ['/usr/include/hunspell',  ]
     main_module_kwargs['extra_compile_args'] = ['-Wall']
 
 main = Extension('hunspell', **main_module_kwargs)
